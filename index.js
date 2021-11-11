9f91865b86b5d0d51136f4b888da3ec806563529
Otavio <otavio_neves@estudante.sesisenai.org.br>
2021-11-10 21:46:35 -0300

    sera se vai

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 8080;
 
 // sendFile will go here
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '/index.html'));
+  res.sendFile(path.join(__dirname, '/menu.html'));
 });
 app.use(express.static(__dirname + '/public'));
 app.listen(port);
